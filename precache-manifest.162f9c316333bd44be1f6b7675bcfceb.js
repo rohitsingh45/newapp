ac185feef6c58fefe4528d04f1eee9f7ca0dd522
Rohit Kumar <rohitkumar252526@gmail.com>
2023-12-28 03:08:46 +0530

    Updates

---
+++ precache-manifest.162f9c316333bd44be1f6b7675bcfceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187162b240dd2545bfd6f3edaf70d96f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5be9c19129fc1e0e6cec",
+    "url": "/my-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "702d9a6e081d1421623e",
+    "url": "/my-app/static/js/2.8b3be0e5.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/my-app/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "5be9c19129fc1e0e6cec",
+    "url": "/my-app/static/js/main.8e8e48e0.chunk.js"
+  },
+  {
+    "revision": "09e0b948311a61c5c54a",
+    "url": "/my-app/static/js/runtime~main.a89e4939.js"
+  }
+]);
